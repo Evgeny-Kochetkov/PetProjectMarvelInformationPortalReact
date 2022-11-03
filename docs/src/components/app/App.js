8001aeb1e853b75113afad03760e08ec33ef2be7
Evgeny Kochetkov <jackeee1993@gmail.com>
2022-11-03 10:41:54 +0300

    before homework

--- docs/src/components/app/App.js
+++ docs/src/components/app/App.js
@@ -3,6 +3,8 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -19,7 +21,7 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
+                {/* <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -29,7 +31,9 @@ const App = () => {
                     <ErrorBoundary>
                         <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
-                </div>
+                </div> */}
+                <AppBanner/>
+                <ComicsList/>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
